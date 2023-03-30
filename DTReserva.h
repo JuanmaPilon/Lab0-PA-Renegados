376eaad10e9f8e81c2fb2f28255807f83ad9b328
Juanma <juanmartin.pilon@gmail.com>
2023-03-30 16:20:58 -0300

    Cambios

--- DTReserva.h
+++ DTReserva.h
@@ -4,21 +4,12 @@
 #include "EstadoReserva.h"
 #include "DTFecha.h"
 
-<<<<<<< HEAD
 
 class DTReserva {
     private:
         int codigo;
         Fecha checkIn;
         Fecha checkOut;
-=======
-class DTReserva {
-    public:
-        int codigo;
-        DTFecha checkIn;
-        DTFecha checkOut;
-        /*Aca no me esta tomando la importacion de DTFecha, no se porque, lo termino de ver el Jueves esto*/
->>>>>>> 758bf1285afdfcf67b3b63333d69e279d7f8a9dd
         EstadoReserva estado;
         float costo;
         int habitacion;
