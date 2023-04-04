586fec1622aa8c613b86a9f2a898a261b122b0f9
Juanma <juanmartin.pilon@gmail.com>
2023-04-04 12:38:09 -0300

    Agreugue la funcion de obtenerHuesped pero no se del todo si esta bien

--- DTSistema.cpp
+++ DTSistema.cpp
@@ -1,4 +1,7 @@
 #include "DTSistema.h"
+#include "DtHabitacion.h"
+#include "Habitacion.h"
+#include <iostream>
 
 void DTSistema::agregarHuesped(string nombre,string email,bool esFinger) {
     for (DTHuesped* huesped : listaHuespedes) {
@@ -10,5 +13,28 @@ void DTSistema::agregarHuesped(string nombre,string email,bool esFinger) {
     listaHuespedes.push_back(nuevoHuesped);
 
     /*La funcion pushback la sacamos de "vector", que basicamente es lo mismo que el .push de JS si 
-    alguno conoce*/
+    alguno conoce la funcion*/
+}
+
+void DTSistema::agregarHabitacion(int numero,float precio,int capacidad) {
+    for (DTHabitacion* habitacion : listaHabitaciones) {
+        if (habitacion->getNumero() == numero) {
+            throw invalid_argument("Ya existe una habitacion registrada con el mismo numero");
+        }
+    }
+    DTHabitacion* nuevaHabtiacion = new DTHabitacion(numero,precio,capacidad);
+    listaHabitaciones.push_back(nuevaHabtiacion);
+}
+
+/*Se usa un ** porque es un arreglo dinamico*/
+
+DTHuesped** DTSistema::obtenerHuespedes(int& cantHuespedes) {
+    DTHuesped** arrHuespedes = new DTHuesped*[MAX_HUESPEDES];
+    int i = 0;
+    for (DTHuesped* huesped : listaHuespedes) {
+        arrHuespedes[i] = huesped;
+        i++;
+    }
+    cantHuespedes = i;
+    return arrHuespedes;
 }
